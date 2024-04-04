390f6d8f02c2908718c72b9647da9734a2bb07e7
ZionPi <503258315@qq.com>
2024-04-04 12:30:23 +0800

    fix references

--- src/CardItem.jsx
+++ src/CardItem.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import Markdown from 'markdown-to-jsx'
 import { motion } from 'framer-motion';
-import {processWithRandomSpeaker,processWithRandomSpeakerForLargeText} from './read_helper'
+// import {processWithRandomSpeaker,processWithRandomSpeakerForLargeText} from './read_helper'
 
 function CardItem({ item }) {
     const textAreaRef = useRef(null);
@@ -55,7 +55,7 @@ function CardItem({ item }) {
             <div className="relative bg-amber-300 flex flex-col justify-center w-[200px] h-[300px] md:w-[400px] md:h-[600px] rounded-2xl shadow-lg hover:shadow-2xl transition-transform duration-300 hover:-translate-y-2.5 
             border-1 border-amber-400 border-opacity-50" >
 
-                <div className="group absolute top-0 right-0 p-2 bg-transparent rounded-bl-lg" onClick={() => processWithRandomSpeakerForLargeText(item.desc)}>
+                <div className="group absolute top-0 right-0 p-2 bg-transparent rounded-bl-lg" onClick={() => {console.log("processWithRandomSpeakerForLargeText(item.desc)")}}>
                     <svg className='w-5 h-5 opacity-0 group-hover:opacity-100' fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
                     </svg>
