2feb5ac4c9d649ff3d22dcd9ee28cf25065336dc
ZionPi <503258315@qq.com>
2024-04-08 16:39:48 +0800

    adjust width for mobile

--- src/ChatComponent.jsx
+++ src/ChatComponent.jsx
@@ -9,7 +9,7 @@ function ChatComponent() {
 
      const messageList = useSelector(state => state.messages.messageList);
 
-    return (<div className="flex flex-col mt-2 w-[400px] md:w-[800px] lg:w-[1340px] lg-relative ">
+    return (<div className="flex flex-col mt-2 w-[380px] md:w-[800px] lg:w-[1340px] lg-relative ">
         <div className="bg-white rounded-lg shadow-md p-4">
             <div className="flex items-center mb-4">
                 <div className="ml-3">
